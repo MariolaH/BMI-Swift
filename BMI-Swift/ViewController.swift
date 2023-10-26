cc87eaca958047b3763b0b0c71b448820cb9b1ae
Mariola <mariolahullings@gmail.com>
2023-10-26 15:14:13 -0400

    Initial Commit

---
+++ BMI-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Swift
+//
+//  Created by Mariola Hullings on 10/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
